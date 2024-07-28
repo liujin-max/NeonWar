6a14cf15dc481f2619e2b3f0bd3d8fb88ba905c4
liujin-max <387616939@qq.com>
2024-07-28 14:12:11 +0800

    产出效率表

--- Assets/Scripts/Logic/Data/NumericalManager.cs
+++ Assets/Scripts/Logic/Data/NumericalManager.cs
@@ -45,54 +45,79 @@ public static class NumericalManager
         return Mathf.FloorToInt(k * hp);
     }
 
+    #region 攻击力成长系数
+    public static int FML_ATK_GC(int level)
+    {
+        return Mathf.FloorToInt((level - 1) / 3) + 1;
+    }
+    #endregion
 
-    //攻击力成长公式
+
+    #region 攻击力成长公式
     //属性 = 初始属性 + (等级 - 1) * 成长系数
     public static int FML_ATK(int level)
     {
         //初始属性
         int base_atk = 1;
         //成长系数
-        int gc = Mathf.FloorToInt((level - 1) / 3) + 1;
+        int gc = FML_ATK_GC(level);
 
         return base_atk + (level - 1) * gc;
     }
+    #endregion
 
-    //攻速成长公式
-    //=$B$4/(1+((A7-1)*D7*$B$5)/100)
+
+    #region 攻速成长系数
+    public static int FML_ASP_GC(int level)
+    {
+        return Mathf.FloorToInt((level - 1) / 3) + 1;
+    }
+    #endregion
+
+
+    #region 攻速成长公式
     public static float FML_ASP(float base_asp, int level)
     {
-        //倍率变化
+        //每级攻速提高1%
         float asp_rate = 1f;
         //成长系数
-        int gc = Mathf.FloorToInt((level - 1) / 3) + 1;
+        int gc = FML_ASP_GC(level);
 
         return base_asp / (1 + ((level - 1) * gc * asp_rate) / 100.0f);
     }
+    #endregion
 
 
-    //升级攻击力消耗的碎片数量和等级的公式
-    public static int FML_ATKCost(int atk_level)
+    #region 攻击消耗公式
+    //消耗=初始消耗*等级^成长指数*成长系数
+    public static int FML_ATKCost(int level)
     {
         //第一次升级消耗的数量
-        int cost_base = 3;
+        int cost_base = 1;
+        //成长指数
+        float power = 2;
+        //成长系数
+        int gc = FML_ATK_GC(level);
 
-        //增长指数
-        float cost_pa = 2f;
 
-        return Mathf.FloorToInt(cost_base * Mathf.Pow(atk_level, cost_pa));
+        return Mathf.FloorToInt(cost_base * Mathf.Pow(level, power) * gc);
     }
+    #endregion
 
-    //升级攻速消耗的碎片数量和等级的公式
-    public static int FML_ASPCost(int asp_level)
+
+
+    #region 攻速消耗公式
+    //消耗=初始消耗*等级^成长指数*成长系数
+    public static int FML_ASPCost(int level)
     {
         //第一次升级消耗的数量
         int cost_base = 2;
+        //成长指数
+        float power = 1.7f;
+        //成长系数
+        int gc = FML_ASP_GC(level);
 
-        //增长指数
-        float cost_pa = 1.6f;
-
-        return Mathf.FloorToInt(cost_base * Mathf.Pow(asp_level, cost_pa));
+        return Mathf.FloorToInt(cost_base * Mathf.Pow(level, power) * gc);
     }
-
+    #endregion
 }
