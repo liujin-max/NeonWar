5b426c637c8c9c1c9fd7f0324c53725101f998c3
liujin-max <387616939@qq.com>
2024-08-09 14:24:40 +0800

    怪物106

--- Assets/Scripts/Logic/Enemy/Enemy_105.cs
+++ Assets/Scripts/Logic/Enemy/Enemy_105.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//树精：高血量且可发射子弹
 public class Enemy_105 : Enemy
 {
     public override void DoAttack()
     {
-        for (int i = 0; i < 6; i++)
+        for (int i = 0; i < 3; i++)
         {
-            float angle = 60.0f * i;
+            float angle = RandomUtility.Random(0, 360);
 
             var bullet = CreateBullet();
             bullet.Shoot(angle);
--- Assets/Scripts/Logic/Enemy/Enemy_106.cs
+++ Assets/Scripts/Logic/Enemy/Enemy_106.cs
@@ -2,31 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//石头人：高血量且可发射子弹
 public class Enemy_106 : Enemy
 {
-    private int m_BuffValue = 3;
-
-    private CDTimer m_Timer = new CDTimer(4f);
-
-    public override void Init(MonsterJSON monster_data)
+    public override void DoAttack()
     {
-        base.Init(monster_data);
-
-        this.AddBuff((int)_C.BUFF.SHIELD, m_BuffValue);
-    }
-
-    public override bool CustomUpdate(float deltaTime)
-    {
-        if (!base.CustomUpdate(deltaTime)) return false;
-
-        m_Timer.Update(deltaTime);
-        if (m_Timer.IsFinished() == true) {
-            m_Timer.Reset();
-
-            if (this.GetBuff((int)_C.BUFF.SHIELD) == null)
-                this.AddBuff((int)_C.BUFF.SHIELD, m_BuffValue);
-        }
-
-        return true;
+        int random = RandomUtility.Random(0, 360);
+        
+        CreateBullet().Shoot(random);
+        CreateBullet().Shoot(random + 120);
+        CreateBullet().Shoot(random - 120);
     }
 }
