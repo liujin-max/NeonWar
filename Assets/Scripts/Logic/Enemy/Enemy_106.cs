bbe2472f70c0064be01ce7079f044b70ea9fe437
liujin-max <387616939@qq.com>
2024-08-11 16:47:11 +0800

    fix 402-106

--- Assets/Scripts/Logic/Enemy/Enemy_106.cs
+++ Assets/Scripts/Logic/Enemy/Enemy_106.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Enemy_106 : Enemy
 {
     public override void DoAttack()
     {
-        int random = RandomUtility.Random(0, 360);
-        
-        CreateBullet().Shoot(random);
-        CreateBullet().Shoot(random + 120);
-        CreateBullet().Shoot(random - 120);
+        float base_angle = ToolUtility.VectorToAngle(Field.Instance.Player.transform.localPosition - transform.localPosition);
+
+        CreateBullet().Shoot(base_angle);
+        CreateBullet().Shoot(base_angle - 45);
+        CreateBullet().Shoot(base_angle + 45);
     }
 }
